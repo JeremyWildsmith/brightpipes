55ab73eb0523bebaace3c26b8e666a3c282debfa
wchoi95 <wchoi95@gmail.com>
2016-05-10 15:16:55 -0700

    Added delta values

--- public_html/js/Direction.js
+++ public_html/js/Direction.js
@@ -1,6 +1,6 @@
 var Direction = {
-    Up : {value: 0, name: "Up" },
-    Down : {value: 0, name: "Up" },
-    Right : {value: 0, name: "Up" },
-    Left : {value: 0, name: "Up" }
+    Up : {value: 0, name: "Up", delta: new Vector(0, -1) },
+    Down : {value: 0, name: "Up", delta: new Vector(0, 1) },
+    Right : {value: 0, name: "Up", delta: new Vector(1, 0) },
+    Left : {value: 0, name: "Up", delta: new Vector(-1, 0) }
 };
