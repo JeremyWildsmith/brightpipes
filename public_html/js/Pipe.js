52310791ac80028874663dce3cf1567a9ff09207
wchoi95 <wchoi95@gmail.com>
2016-05-10 15:18:03 -0700

    Created getConnections and getDirections functions. Started working on the fill function.

--- public_html/js/Pipe.js
+++ public_html/js/Pipe.js
@@ -3,12 +3,23 @@ function Pipe(parentGrid, graphic, connectionDirections, location)
     this.location = location;
     this.parentGrid = parentGrid;
     this.graphic = graphic;
-    this.connnectionDirections = connectionDirections;
+    this.connectionDirections = connectionDirections;
+    this.filled = false;
+    this.leaked = false;
 }
 
 //Set pipe fill state to full, or if already full, fill out pipes
 Pipe.prototype.fill = function() {
-    
+    if (this.filled === true) {
+        var pipeArray = getConnections();
+        
+        if (pipeArray.length === 0) {
+            this.leaked = true;
+        } else {
+            
+        }
+    } else 
+        this.filled = true;
 };
 
 //Draw the pipe graphic
@@ -16,11 +27,59 @@ Pipe.prototype.draw = function(g, x, y) {
     
 };
 
-Pipe.prototype.getConnections = function(g, x, y) {
+Pipe.prototype.getConnections = function() {
+    var pipeArray = [];
+    var pipeUp = 
+            this.parentGrid.getPipe(this.location.add(Direction.Up.delta));
+    var pipeDown = 
+            this.parentGrid.getPipe(this.location.add(Dirction.Down.delta));
+    var pipeRight = 
+            this.parentGrid.getPipe(this.location.add(Direction.Right.delta));
+    var pipeLeft = 
+            this.parentGrid.getPipe(this.location.add(Direction.Left.delta()));
+    
+    for(i = 0; i < this.connectionDirections.length; i++) {
+        switch(this.connectionDirections[i]) {
+            case this.connectionDirection.Up:
+                if (pipeUp !== null && 
+                        pipeUp.getDirections.indexOf(Direction.Down) !== -1) {
+                    pipeArray.push(pipeUp);
+                }
+                break;
+                
+            case this.connectionDirection.Down:
+                if (pipeDown !== null && 
+                        pipeDown.getDirections.indexOf(Direction.Up) !== -1) {
+                    pipeArray.push(pipeDown);
+                }
+                break;
+            
+            case this.connectionDirection.Right:
+                if (pipeRight !== null &&
+                        pipeRight.getDirections.indexOf(Direction.Right) !== -1) {
+                    pipeArray.push(pipeRight);
+                }
+             
+            case this.connectionDirection.Left:
+                if (pipeLeft !== null &&
+                        pipeLeft.getDirections.indexOf(Direction.Left) !== -1) {
+                    pipeArray.push(pipeLeft);
+                }
+        }
+    }
+    
+    return pipeArray;
     
 };
-
 //Probably wont do much, at least not now
 Pipe.prototype.update = function(deltaTime) {
     
+};
+
+Pipe.prototype.connectedToPump = function() {
+    
+};
+
+Pipe.prototype.getDirections = function() {
+    return this.connectionDirections;
 };
