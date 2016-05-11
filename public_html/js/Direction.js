3b10decb41eda2c85c396998e4700cc4d2cfc6e9
StevenOrola <stevenorola@gmail.com>
2016-05-11 14:21:09 -0700

    fixed values for enum

--- public_html/js/Direction.js
+++ public_html/js/Direction.js
@@ -1,6 +1,6 @@
 var Direction = {
     Up : {value: 0, name: "Up", delta: new Vector(0, -1) },
-    Down : {value: 0, name: "Up", delta: new Vector(0, 1) },
-    Right : {value: 0, name: "Up", delta: new Vector(1, 0) },
-    Left : {value: 0, name: "Up", delta: new Vector(-1, 0) }
+    Down : {value: 1, name: "Up", delta: new Vector(0, 1) },
+    Right : {value: 2, name: "Up", delta: new Vector(1, 0) },
+    Left : {value: 3, name: "Up", delta: new Vector(-1, 0) }
 };
