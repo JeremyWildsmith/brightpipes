a5cb60525586245a01b7bb25022272a71a2bf890
Jeremy Wildsmith <jeremywildsmith@yahoo.ca>
2016-05-12 09:03:22 -0700

    Added mouse listeners.

--- public_html/js/Main.js
+++ public_html/js/Main.js
@@ -3,18 +3,47 @@ var CANVAS_HEIGHT = 600;
 
 var world;
 
-window.onload = function() {
-    var dt = 1000/30;
-    var canvas = document.getElementById("mainCanvas").getContext("2d");
+var cursorLocation = new Vector(0, 0);
+var canvas = null;
+var game = null;
+
+function getLocation(e) {
+    var x = 0;
+    var y = 0;
+
+    if (e.pageX || e.pageY) {
+        x = e.pageX;
+        y = e.pageY;
+    }
+    else {
+        x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+        y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+    }
+
+    x -= canvas.offsetLeft;
+    y -= canvas.offsetTop;
+
+    return new Vector(x, y);
+}
+
+window.onload = function () {
+    var dt = 1000 / 30;
+    canvas = document.getElementById("mainCanvas");
+    var g = canvas.getContext("2d");
     game = new Game(CANVAS_WIDTH, CANVAS_HEIGHT);
 
     var last = Date.now();
-    setInterval(function() {
-      var now = Date.now();
-      var deltaTime = now - last;
-      last = now;
-      game.update(deltaTime);
-      game.draw(canvas, 0, 0);
+    setInterval(function () {
+        var now = Date.now();
+        var deltaTime = now - last;
+        last = now;
+        game.update(deltaTime);
+        game.draw(g, 0, 0);
     }, dt);
-    
+};
+
+
+document.onmousemove = function (e) {
+    game.update(0);
+    game.onMouseMove(getLocation(e));
 };
