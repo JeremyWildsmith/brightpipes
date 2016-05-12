666593ac73841ef8ba092885ab972b020dfb80da
Jeremy <Jeremy@DESKTOP-I9KVITV>
2016-05-11 22:03:37 -0700

    Added values method to get all possible directions.

--- public_html/js/Direction.js
+++ public_html/js/Direction.js
@@ -2,5 +2,13 @@ var Direction = {
     Up : {value: 0, name: "Up", delta: new Vector(0, -1) },
     Down : {value: 1, name: "Up", delta: new Vector(0, 1) },
     Right : {value: 2, name: "Up", delta: new Vector(1, 0) },
-    Left : {value: 3, name: "Up", delta: new Vector(-1, 0) }
+    Left : {value: 3, name: "Up", delta: new Vector(-1, 0) },
+    values: function() {
+        var array = [];
+        array.push(Direction.Up);
+        array.push(Direction.Down);
+        array.push(Direction.Right);
+        array.push(Direction.Left);
+        return array;
+    }
 };
