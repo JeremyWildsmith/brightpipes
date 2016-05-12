36e072be9faa7bc323d7bd4831d5e44fd6ab73a6
StevenOrola <stevenorola@gmail.com>
2016-05-11 17:29:08 -0700

    Revert "Revert "Revert "Allowed for grid size to be specified through constructor."""
    This reverts commit 177ba9bf57b07d9f9d67b19ad0e4e1139e37282c.

--- public_html/js/Grid.js
+++ public_html/js/Grid.js
@@ -1,17 +1,17 @@
-function Grid(cellDimensions, gridWidth, gridHeight) {
-    this.gridWidth = gridWidth;
-    this.gridHeight = gridHeight;
+function Grid(cellDimensions) {
+    this.GRID_WIDTH = 5;
+    this.GRID_HEIGHT = 5;
     
     this.cellDimensions = cellDimensions;
     
     this.pipes = [];
     this.obstacles = [];
     
-    for(var x = 0; x < this.gridWidth; x++) {
+    for(var x = 0; x < this.GRID_WIDTH; x++) {
         this.pipes[x] = [];
         this.obstacles[x] = [];
         
-        for(var y = 0; y < this.gridWidth; y++) {
+        for(var y = 0; y < this.GRID_WIDTH; y++) {
             this.pipes[x][y] = null;
             this.obstacles[x][y] = null;
         }
@@ -19,7 +19,7 @@ function Grid(cellDimensions, gridWidth, gridHeight) {
 }
 
 Grid.prototype.validateLocation = function(location) {
-    if(location.x > this.gridWidth || location.y > this.gridHeight ||
+    if(location.x > this.GRID_WIDTH || location.y > this.GRID_HEIGHT ||
        location.x < 0 || location.y > 0)
    {
        throw "Provided location it outside of grid bounds.";
@@ -51,8 +51,8 @@ Grid.prototype.clearPipe = function(location) {
 Grid.prototype.screenToGrid = function(location) {
     var translation = new Vector(location.x / this.cellDimensions, location.y / this.cellDimensions);
     
-    if(translation.x < 0 || translation.x > this.gridWidth ||
-       translation.y < 0 || translation.y > this.gridHeight)
+    if(translation.x < 0 || translation.x > this.GRID_WIDTH ||
+       translation.y < 0 || translation.y > this.GRID_HEIGHT)
         throw "Specified coordinates are not inside screen bounds.";
     
     return translation;
@@ -60,8 +60,8 @@ Grid.prototype.screenToGrid = function(location) {
 
 Grid.prototype.draw = function(g, x, y) {
     g.strokeStyle = "#000";
-    for(var xLoc = 0; xLoc < this.gridWidth; xLoc++) {   
-        for(var yLoc = 0; yLoc < this.gridHeight; yLoc++) {
+    for(var xLoc = 0; xLoc < this.GRID_WIDTH; xLoc++) {   
+        for(var yLoc = 0; yLoc < this.GRID_HEIGHT; yLoc++) {
             g.rect(x + xLoc * this.cellDimensions,
                   y + yLoc * this.cellDimensions,
                   this.cellDimensions,
@@ -71,8 +71,8 @@ Grid.prototype.draw = function(g, x, y) {
         }
     }
     
-    for(var xLoc = 0; xLoc < this.gridWidth; xLoc++) {   
-        for(var yLoc = 0; yLoc < this.gridWidth; yLoc++) {
+    for(var xLoc = 0; xLoc < this.GRID_WIDTH; xLoc++) {   
+        for(var yLoc = 0; yLoc < this.GRID_WIDTH; yLoc++) {
             if(this.pipes[xLoc][yLoc] === null)
                 continue;
             
