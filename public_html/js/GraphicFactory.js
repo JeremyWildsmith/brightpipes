b1724528705f1a02820dc68de906d3cbfb2b38c3
Jeremy <Jeremy@DESKTOP-I9KVITV>
2016-05-11 22:03:53 -0700

    Defaulted origin to centre of graphic.

--- public_html/js/GraphicFactory.js
+++ public_html/js/GraphicFactory.js
@@ -4,8 +4,9 @@ function GraphicFactory() {
 
 GraphicFactory.prototype.createFromFile = function(path, callback) {
     var img = new Image();
+    
     img.onload = function() {
-        callback(new Graphic(img, 0, 0, 0, 0, img.width, img.height));
+        callback(new Graphic(img, img.width / 2, img.height / 2, 0, 0, img.width, img.height));
     };
     
     img.src = path;
