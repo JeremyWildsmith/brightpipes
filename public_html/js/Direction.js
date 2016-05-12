d6267dab0fff88dca2d819d7eac3ffc1ea552c31
Jeremy Wildsmith <jeremywildsmith@yahoo.ca>
2016-05-12 10:10:23 -0700

    Fixed direction enum names.

--- public_html/js/Direction.js
+++ public_html/js/Direction.js
@@ -1,8 +1,8 @@
 var Direction = {
     Up : {value: 0, name: "Up", delta: new Vector(0, -1) },
-    Down : {value: 1, name: "Up", delta: new Vector(0, 1) },
-    Right : {value: 2, name: "Up", delta: new Vector(1, 0) },
-    Left : {value: 3, name: "Up", delta: new Vector(-1, 0) },
+    Down : {value: 1, name: "Down", delta: new Vector(0, 1) },
+    Right : {value: 2, name: "Left", delta: new Vector(1, 0) },
+    Left : {value: 3, name: "Right", delta: new Vector(-1, 0) },
     values: function() {
         var array = [];
         array.push(Direction.Up);
