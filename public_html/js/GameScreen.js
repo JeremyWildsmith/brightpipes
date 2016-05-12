48df68ebd8d69bc97dd7f8d79403777f44e0e972
Jeremy <Jeremy@DESKTOP-I9KVITV>
2016-05-11 17:07:54 -0700

    Add render calls for grid and pipe selection queue.

--- public_html/js/GameScreen.js
+++ public_html/js/GameScreen.js
@@ -1,16 +1,19 @@
 function GameScreen() {
     this.PUMP_INTERVAL = 20000;
+    this.CELL_DIMENSIONS = 40;
 
     this.elapsedSinceLastPump = 0;
 
-    this.grid = new Grid();
+    this.grid = new Grid(this.CELL_DIMENSIONS, 5, 5);
+    this.pipeSelection = new PipeSelectionQueue();
+    
 
 }
 
 GameScreen.prototype.update = function (deltaTime) {
     this.elapsedSinceLastPump += deltaTime;
     
-    if (this.elapsedSinceLastPump > PUMP_INTERVAL)
+    if (this.elapsedSinceLastPump > this.PUMP_INTERVAL)
     {
         this.elapsedSinceLastPump = 0;
         this.grid.pump();
@@ -18,9 +21,12 @@ GameScreen.prototype.update = function (deltaTime) {
 };
 
 GameScreen.prototype.draw = function (g, x, y) {
-    var canvas = document.getElementById("mainCanvas");
     g.font = "15px Arial";
-    g.fillText("Score: ", 10, 100);  //missing the function that counts the score
-    g.fillText("Number of pipes used: ", 10, 120); // missing the function that counts the number of pipes used
+    g.fillText("Next Pump In: " + Math.round((this.PUMP_INTERVAL - this.elapsedSinceLastPump) / 1000) + "s", 10, 15);  //missing the function that counts the score
+    g.fillText("Number of pipes used: ", 10, 35); // missing the function that counts the number of pipes used
+
+    this.grid.draw(g, 10, 55);
+    this.pipeSelection.draw(g, 10, 270);
+
 };
 
