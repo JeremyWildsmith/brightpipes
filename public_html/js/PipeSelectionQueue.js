a5ee21484954f16ab76c9e2ba136767e83bad05b
Jeremy <Jeremy@DESKTOP-I9KVITV>
2016-05-11 17:23:52 -0700

    Added interface for PipeSelection queue.

---
+++ public_html/js/PipeSelectionQueue.js
@@ -0,0 +1,15 @@
+function PipeSelectionQueue() {
+    this.pipeGrid = new Grid(50, 3, 1);
+}
+
+PipeSelectionQueue.prototype.draw = function(g, x, y) {
+    this.pipeGrid.draw(g, x, y);
+};
+
+PipeSelectionQueue.prototype.popPipe = function(location) {
+    
+};
+
+PipeSelectionQueue.prototype.pushPipe = function(pipe) {
+    
+};
