086d1e843217dadf2eb18eff8d2e8ba067d9f9d8
Jeremy <Jeremy@DESKTOP-I9KVITV>
2016-05-11 21:58:23 -0700

    Added pipes enum and factory methods.

--- public_html/js/Pipes.js
+++ public_html/js/Pipes.js
@@ -1,14 +1,70 @@
 var Pipes = {
     Vertical: {value: 0, create: function() {
+            var dirs = [];
             
+            dirs.push(Direction.Up);
+            dirs.push(Direction.Down);
+            
+            var graphic = new LoadingGraphic("gfx/vertical.png");
+            
+            return new Pipe(graphic, dirs);
     }},
-    Horizontal: {},
+
+    Horizontal: {value: 1, create: function() {
+            var dirs = [];
+            
+            dirs.push(Direction.Left);
+            dirs.push(Direction.Right);
+            
+            var graphic = new LoadingGraphic("gfx/horizontal.png");
+            
+            return new Pipe(graphic, dirs);
+    }},
     
-    RightDown: {},
-    LeftDown: {},
+    RightDown: {value: 2, create: function() {
+            var dirs = [];
+            
+            dirs.push(Direction.Right);
+            dirs.push(Direction.Down);
+            
+            var graphic = new LoadingGraphic("gfx/rightDown.png");
+            
+            return new Pipe(graphic, dirs);
+    }},
+
+    LeftDown: {value: 3, create: function() {
+            var dirs = [];
+            
+            dirs.push(Direction.Left);
+            dirs.push(Direction.Down);
+            
+            var graphic = new LoadingGraphic("gfx/rightDown.png");
+            
+            return new Pipe(graphic, dirs);
+    }},
     
-    RightUp: {},
-    LeftUp: {},
+    RightUp: {value: 4, create: function() {
+            var dirs = [];
+            
+            dirs.push(Direction.Right);
+            dirs.push(Direction.Up);
+            
+            var graphic = new LoadingGraphic("gfx/rightUp.png");
+            
+            return new Pipe(graphic, dirs);
+    }},
+
+    LeftUp: {value: 5, create: function() {
+            var dirs = [];
+            
+            dirs.push(Direction.Left);
+            dirs.push(Direction.Up);
+            
+            var graphic = new LoadingGraphic("gfx/leftUp.png");
+            
+            return new Pipe(graphic, dirs);
+    }},
+
     values: function() {
         var array = [];
         
