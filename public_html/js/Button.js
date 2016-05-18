85fa9796c42547e3e9841e2e802ba1d46c4aec25
wchoi95 <wchoi95@gmail.com>
2016-05-17 19:30:01 -0700

    First creation of Button.js. Label part might need further editing after test.

--- public_html/js/Button.js
+++ public_html/js/Button.js
@@ -1,17 +1,30 @@
-function Button(label, callback) {
+function Button(defaultGraphic, hoverGraphic, label, callback) {
+    this.defaultGraphic = defaultGraphic;
+    this.hoverGraphic = hoverGraphic;
+    this.label = label;
     this.callback = callback;
+    this.hovering = false;
 }
 
 Button.prototype.draw = function(g, x, y) {
-    this.graphic.draw(g, x, y);
+    g.font = "15px Arial";
+    g.fillText(label, x, y);
+    
+    if (this.hovering) {
+        this.hoverGraphic.draw(g, x, y);
+    } else {
+        this.defaultGraphic.draw(g, x, y);
+    }
 };
 
 Button.prototype.mouseEnter = function() {
     //Go to hover graphic
+    this.hovering = true;
 };
 
 Button.prototype.mouseLeave = function() {
     //Go to default graphic
+    this.hovering = false;
 };
 
 Button.prototype.mouseDown = function(location) {
